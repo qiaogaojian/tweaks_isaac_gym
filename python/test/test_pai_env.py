0203d7a66199002065af057dd7d76cb79e668145
qiaogaojian <qiaogaojian@vip.qq.com>
2024-06-24 17:14:10 +0800

    feat: pai env test script

--- python/test/test_pai_env.py
+++ python/test/test_pai_env.py
@@ -1,36 +1,26 @@
-import time
-
-from pycore.logger import Logger
-
 from humanoid.envs import *
 from humanoid.utils import get_args, task_registry
-import torch
-
-# ********************* step1 init env *********************
 from humanoid.utils.time_utils import get_timestamp
 
+import torch
+from pycore.logger import Logger
+
+# ********************* config *********************
 args = get_args()
 args.num_envs = 1
+
+# *********************  env *********************
 env, env_cfg = task_registry.make_env(name=args.task, args=args)
 device = env.device
 print(f"env: {type(env_cfg)}")
 
-# ********************* action queue *********************
-action1 = torch.Tensor([
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0,
-    0, 0, 0
-]).to(device)
+# ********************* action *********************
+action1 = torch.Tensor([0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]).to(device)
 actions1 = action1.unsqueeze(0)
 
-action2 = torch.Tensor([
-    0, 0, -1,
-    -3, -1, 0,
-    0, 0, 1,
-    3, 1, 0
-]).to(device)
+action2 = torch.Tensor([0, 0, -1, -2, -1, 0, 0, 0, 1, 2, 1, 0]).to(device)
 action2 = action2.unsqueeze(0)
+
 action_queue = [action1, action2]
 
 # ********************* render *********************
@@ -42,15 +32,11 @@ last_switch_time = get_timestamp()
 while True:
     # v1
     _now = get_timestamp()
-    if _now - last_switch_time > switch_freq:  # 1s
+    if _now - last_switch_time > switch_freq:  # 1000 ms
         target_q = action_queue[gym_step % 2] * 1
         Logger.instance().info(f"切换发送目标 {target_q}")
-        gym_step += 1
-        last_switch_time = _now
 
-    # v2
-    # target_q = action_queue[gym_step % 2] * 1
-    # Logger.instance().info(f"切换发送目标 {target_q}")
-    # gym_step += 1
+        last_switch_time = _now
+        gym_step += 1
 
     env.step(target_q)
